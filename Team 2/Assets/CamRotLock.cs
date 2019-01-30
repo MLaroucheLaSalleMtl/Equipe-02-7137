3c4cc46a36cc9889550f2d3abe076b1b2a269d23
Adrien Jeanson <Adrilus3000@live.ca>
2019-01-30 11:03:48 -0500

    Player movement
    Added player movement on nav mesh map

---
+++ Team 2/Assets/CamRotLock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class CamRotLock : MonoBehaviour
+{
+    Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+    
+    //EarlyUpdate 
+}
---
+++ Team 2/Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+
+    private NavMeshAgent agent;
+    private Vector3 destination;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target.transform.position = transform.position;
+        agent = GetComponent<NavMeshAgent>();
+        destination = agent.destination;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Move"))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+            target.transform.position = hit.point;
+            destination = target.transform.position;
+            agent.destination = destination;
+
+
+        }
+
+    }
+   
+}
