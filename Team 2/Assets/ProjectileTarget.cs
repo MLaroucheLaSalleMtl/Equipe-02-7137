e1ad2a64778498e1c6edc9115951dfd3e3bc892a
Adrien Jeanson <Adrilus3000@live.ca>
2019-02-04 16:41:57 -0500

    projectile for basic attack

--- Team 2/Assets/ProjectileTarget.cs
+++ Team 2/Assets/ProjectileTarget.cs
@@ -6,11 +6,12 @@ public class ProjectileTarget : MonoBehaviour
 {
     public Transform target;
     [SerializeField] float speed=2;
+    public Transform owner;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 2;
     }
 
     // Update is called once per frame
@@ -22,9 +23,15 @@ public class ProjectileTarget : MonoBehaviour
 
     private void Move()
     {
-        Vector3 direction = transform.position - target.transform.position;
+        Vector3 direction =target.transform.position- transform.position;
         transform.position += direction.normalized * speed * Time.deltaTime;
 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<OnHit>().Hit(owner);
+        Destroy(this);
     }
 
 }
