dd793610410a5772b15cdfaca123a85532b97f51
Samuel Paquette <samuelpaquette@hotmail.fr>
2019-02-11 04:40:11 -0500

    Samuel Paquette @ 11 FEB 2019: GameManager / Start of designs
    +GameManager
    +NPCHandler
    +World position
    +Entity interface
    +Player interface
    +NPC interface

---
+++ Team 2/Assets/Entities/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// @author Samuel Paquette
+/// @date 1 February 2019
+/// @description The abstract design of an entity in the game
+/// </summary>
+public interface Entity
+{
+
+    protected int hp;
+    protected Position worldPos;
+    protected int level;
+    protected string name;
+
+    public abstract int HP { get; }
+    public abstract Position WorldPosition { get; set; }
+    public abstract int Level { get; }
+    public abstract string Name { get; protected set; }
+
+    public abstract void GiveHp(int amount);
+    public abstract void RemoveHp(int amount);
+    public abstract void LevelUp();
+    public abstract void LevelDown();
+    public abstract void SetLevel(int level);
+    public abstract void Move(Position newPosition);
+
+}
---
+++ Team 2/Assets/Entities/Npcs/NPC.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// @author Samuel Paquette
+/// @date 1 February 2019
+/// @description The abstract design of a non playable character.
+/// </summary>
+public class NPC : MonoBehaviour
+{
+
+    /// <summary>
+    /// Called when the NPC is created
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Called once per game tick after the NPC is spawned
+    /// </summary>
+    void Update()
+    {
+        
+    }
+
+}
