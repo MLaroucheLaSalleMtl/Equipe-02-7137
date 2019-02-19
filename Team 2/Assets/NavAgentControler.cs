3cfcc74d88680e2abbb134247c0e8147022bbd3e
Adrien Jeanson <Adrilus3000@live.ca>
2019-02-18 21:00:11 -0500

    added nav mesh agent script for monsters

---
+++ Team 2/Assets/NavAgentControler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentControler : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    [SerializeField] float range;
+    [SerializeField] GameObject[] targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject closest=null;
+        float distance=float.MaxValue;
+
+        foreach(GameObject g in targets)
+        {
+            float d = Vector3.Distance(transform.position, g.transform.position);
+            if (d<range&&d<distance)
+            {
+                distance = d;
+                closest = g;
+            }
+        }
+        if(closest!=null)
+        agent.SetDestination(closest.transform.position);
+    }
+}
