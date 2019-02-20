15ebc68058345bd511d6bad828ce2b4a0837b6d3
Samuel Paquette <samuelpaquette@hotmail.fr>
2019-02-19 20:20:09 -0500

    Auto stash before merge of "master" and "origin/master"

--- Team 2/Assets/Entities/Npcs/Factory/GoblinFactory.cs
+++ Team 2/Assets/Entities/Npcs/Factory/GoblinFactory.cs
@@ -1,11 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//https://www.c-sharpcorner.com/article/factory-method-design-pattern-in-c-sharp/
+
+/// <summary>
+/// @author Samuel Paquette
+/// @date 15 FEB 19
+/// @description The creator of goblins
+/// </summary>
 public class GoblinFactory : MonsterFactory
 {
+    /// <summary>
+    /// Fields
+    /// </summary>
+    private readonly int instanceId;
+    private readonly string displayName;
+    private readonly int level;
+    private readonly int maxHp;
+    private readonly int currentHp;
+
+    /// <summary>
+    /// Constructor for the creator
+    /// </summary>
+    /// <param name="instanceId"></param>
+    /// <param name="displayName"></param>
+    /// <param name="level"></param>
+    /// <param name="maxHp"></param>
+    /// <param name="currentHp"></param>
+    public GoblinFactory (int instanceId, string displayName, int level, int maxHp, int currentHp)
+    {
+        this.instanceId = instanceId;
+        this.displayName = displayName;
+        this.level = level;
+        this.maxHp = maxHp;
+        this.currentHp = currentHp;
+    }
+
+    /// <summary>
+    /// Creates a goblin
+    /// </summary>
+    /// <returns></returns>
     public override NPC GetNPC()
     {
-        throw new System.NotImplementedException();
+        return new Goblin(instanceId, displayName, level, maxHp, currentHp);
     }
+
 }
