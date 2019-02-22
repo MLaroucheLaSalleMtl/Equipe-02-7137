5591362529822be41480c055fa0703581f626185
Samuel Paquette <samuelpaquette@hotmail.fr>
2019-02-22 02:52:00 -0500

    Samuel Paquette @ 21 FEB 2019: Start of Warrior Class
    +Warrior class

---
+++ Team 2/Assets/Entities/Player/Classes/WarriorClass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// @author Samuel Paquette
+/// @date 21 FEB 19
+/// @description The warrior class (close combat)
+/// </summary>
+public abstract class WarriorClass : PlayerClass
+{
+
+
+    public WarriorClass ()
+    {
+        Range = 2;
+        IsMelee = true;
+        Spells = new Dictionary<int, Spell>();
+        DisplayName = "Warrior";
+        Description = "A close combat class which is tankier than others.";
+        UnlockedSkills = new List<Skill>();
+    }
+
+}
