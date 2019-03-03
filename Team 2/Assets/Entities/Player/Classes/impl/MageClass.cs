909cd43de23bd432506bb8e30cb50b1becd5e110
Unknown <yassineougouag@gmail.com>
2019-03-03 12:56:50 -0500

    Refixed the stats, added a level up function
    Need to implement a script on enemies that gives xp thats stackable on the player

--- Team 2/Assets/Entities/Player/Classes/impl/MageClass.cs
+++ Team 2/Assets/Entities/Player/Classes/impl/MageClass.cs
@@ -7,27 +7,41 @@ using System.Threading.Tasks;
 
 class MageClass : PlayerClass
 {
-    private double range;
-    private bool isMelee;
     private Dictionary<int, Spell> spells;
     private string displayName;
     private string description;
-    public override double Range { get => range; set => range = value; }
-    public override bool IsMelee { get => isMelee;  set => isMelee = value; }
-    public override Dictionary<int, Spell> Spells { get => spells;  set => spells = value; }
-    public override string DisplayName { get => displayName;  set => displayName = value; }
-    public override string Description { get => description;  set => description = value; }
-    public override List<Skill> UnlockedSkills { get => UnlockedSkills; set => UnlockedSkills = value; }
-    public override ClassesInformation.ClassesId Id { get; set; }
-
+    private int level;
+    private double strengh;
+    private double agility;
+    private double intelligence;
+    private bool isMelee;
+    public override bool IsMelee { get => isMelee; set => isMelee = value; }
+    public override Dictionary<int, Spell> Spells { get => spells; set => spells = value; }
+    public override string DisplayName { get => displayName; set => displayName = value; }
+    public override string Description { get => description; set => description = value; }
+    public override int Level { get => level; set => level = value; }
+    public override double Strengh { get => strengh; set => strengh = value; }
+    public override double Agility { get => agility; set => agility = value; }
+    public override double Intelligence { get => intelligence; set => intelligence = value; }
     public MageClass()
     {
-        Range = 5;
+   
         IsMelee = false;
-        Id = ClassesInformation.ClassesId.MAGE;
         Spells = new Dictionary<int, Spell>();
         DisplayName = "Mage";
         Description = "A ranged class with intelligence as main stat";
+        Level = 1;
+        Agility = 1;
+        Strengh = 1;
+        Intelligence = 3;
     }
 
+    public override void Levelup()
+    {
+        level++;
+        Intelligence++;
+        Intelligence++;
+        Agility++;
+        Strengh++;
+    }
 }
--- Team 2/Assets/Entities/Player/Classes/impl/RangerClass.cs
+++ Team 2/Assets/Entities/Player/Classes/impl/RangerClass.cs
@@ -1,32 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class RangerClass : PlayerClass
 {
-    private double range;
+    
     private bool isMelee;
     private Dictionary<int, Spell> spells;
     private string displayName;
     private string description;
+    private int level;
 
-    public override double Range { get => range; set => range = value; }
+    private double strengh;
+    private double agility;
+    private double intelligence;
+
+   
     public override bool IsMelee { get => isMelee; set => isMelee = value; }
     public override Dictionary<int, Spell> Spells { get => spells; set => spells = value; }
     public override string DisplayName { get => displayName; set => displayName = value; }
     public override string Description { get => description; set => description = value; }
-    public override List<Skill> UnlockedSkills { get; set; }
-    public override ClassesInformation.ClassesId Id { get; set; }
-
+    public override int Level { get => level; set => level = value; }
+    public override double Strengh { get => strengh; set => strengh = value; }
+    public override double Agility { get => agility; set => agility = value; }
+    public override double Intelligence { get => intelligence; set => intelligence = value; }
     public RangerClass()
     {
-        Range = 10;
+       
         IsMelee = false;
-        Id = ClassesInformation.ClassesId.RANGER;
         Spells = new Dictionary<int, Spell>();
         DisplayName = "Ranger";
         Description = "The Ranger is an archer type class who uses bows as its weapon and agility as its main stat";
+        Level = 1;
+        Agility = 3;
+        Strengh = 1;
+        Intelligence = 1;
     }
-
+    public override void Levelup()
+    {
+        level++;
+        Intelligence++;
+        Agility++;
+        Agility++;
+        Strengh++;
+    }
 }
-
