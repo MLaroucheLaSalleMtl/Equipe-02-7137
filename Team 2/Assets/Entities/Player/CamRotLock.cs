2c431586e0843a527d3bc0bd80d98b16bcf56d75
Adrien Jeanson <Adrilus3000@live.ca>
2019-03-06 09:17:09 -0500

    zoom
    scrollwheel camera zoom (op)

--- Team 2/Assets/Entities/Player/CamRotLock.cs
+++ Team 2/Assets/Entities/Player/CamRotLock.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class CamRotLock : MonoBehaviour
 {
     Quaternion rotation;
-
+    [SerializeField] Transform rail;
+    [SerializeField] float maxZoom = -5;
+    [SerializeField] float minZoom = -12;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,18 @@ public class CamRotLock : MonoBehaviour
     void Update()
     {
         transform.rotation = rotation;
+        if(rail.localPosition.z<= maxZoom && rail.localPosition.z>= minZoom)
+        {
+            rail.transform.localPosition=new Vector3(rail.transform.localPosition.x, rail.transform.localPosition.y, rail.transform.localPosition.z+ Input.GetAxis("Zoom"));
+        }
+        if(rail.localPosition.z > maxZoom)
+        {
+            rail.transform.localPosition = new Vector3(rail.transform.localPosition.x, rail.transform.localPosition.y, maxZoom);
+        }
+        else if(rail.localPosition.z < minZoom)
+        {
+            rail.transform.localPosition = new Vector3(rail.transform.localPosition.x, rail.transform.localPosition.y, minZoom);
+        }
     }
     
-    //EarlyUpdate 
 }
