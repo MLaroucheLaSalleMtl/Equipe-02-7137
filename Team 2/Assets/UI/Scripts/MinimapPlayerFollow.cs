c1d73b4e225b554b66bdfdfd7111796754204275
Samuel Paquette <samuelpaquette@hotmail.fr>
2019-03-23 19:00:32 -0400

    Samuel Paquette @ 23 MAR 2019 : Small fix on minimap
    +Minimap camera now follows the player

---
+++ Team 2/Assets/UI/Scripts/MinimapPlayerFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// @author Samuel Paquette
+/// @date 23 MARS 2019
+/// @description Minimap camera follows the player position
+/// </summary>
+public class MinimapPlayerFollow : MonoBehaviour
+{
+
+    public GameObject player;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.localPosition.x, transform.position.y, player.transform.localPosition.z);    
+    }
+}
