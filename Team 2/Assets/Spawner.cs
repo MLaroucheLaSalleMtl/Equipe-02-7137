c324e8d713618bc4be40c8e504fd9d26a332783c
Adrien Jeanson <Adrilus3000@live.ca>
2019-03-25 08:28:13 -0400

    fix spawner

--- Team 2/Assets/Spawner.cs
+++ Team 2/Assets/Spawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] MonsterHandler mHandler;
+    GameManager manager;
     [SerializeField] int spawnRange=10;
     [SerializeField] float playerRange = 50;
     [SerializeField] float minSpawnTime = 5;
@@ -14,13 +14,14 @@ public class Spawner : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         //gManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         //if(player=null)
         //{
         //    player = GameObject.Find("Player").GetComponent<Transform>();
         //}
+        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
         Spawn();
     }
 
@@ -38,7 +39,7 @@ public class Spawner : MonoBehaviour
             int monster = Random.Range(0, type.Length - 1);
             int spawnX = Random.Range(-spawnRange, spawnRange);
             int spawnZ = Random.Range(-spawnRange, spawnRange);
-            mHandler.SpawnMonster(type[monster], new Position(spawnX, (int)transform.position.y, spawnZ), type[monster].ToString(), 1, 5, 5);
+            manager.monsterHandler.SpawnMonster(type[monster], new Position(spawnX+ (int)transform.position.x, (int)transform.position.y, spawnZ+ (int)transform.position.z), type[monster].ToString(), 1, 5, 5);
         }
         Invoke("Spawn", time);
     }
