916c5d19f72f06928ecd4859b1407691642c33c8
Unknown <yassineougouag@gmail.com>
2019-03-30 16:14:09 -0400

    MainMenu
    Main menu with option menu, copyrighted music for testing

---
+++ Team 2/Assets/MainMenuStuff/OptionsMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer Music;
+    // Start is called before the first frame update
+    public void SetMusicVolume(float MusicVolume)
+    {
+        Music.SetFloat("MusicVolume", MusicVolume);
+
+    }
+
+   
+}
