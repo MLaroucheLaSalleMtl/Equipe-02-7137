b383ddfd950a153ca1b48ef10681699913d3732a
Samuel Paquette <samuelpaquette@hotmail.fr>
2019-04-18 02:00:16 -0400

    Samuel Paquette @ 17 AVR 19 : Added back buttons to UI (panels)
    +Back buttons

---
+++ Team 2/Assets/UI/Scripts/ExitGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/*
+ * @author Marc-Andre Larouche
+ * @description Exit the game
+ */
+public class ExitGame : MonoBehaviour
+{
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();  
+#endif
+    }
+
+}
